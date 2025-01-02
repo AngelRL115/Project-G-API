05a55f6e21bcf30364ecc8d877fa85db37ad92e6
angel0las0 <angelopolis1_2009@hotmail.com>
2025-01-02 04:11:17 -0600

    DB, ORM, scripts, router, swagger
    all those features were added to the project and working good to go

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import baseRouter from './routes/baseRouter'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsdoc = require('swagger-jsdoc')
 import * as dotenv from 'dotenv'
 
 dotenv.config()
@@ -19,6 +22,43 @@ app.use(logger)
 app.use(bodyParser.json({ limit: '100mb' }))
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
 app.disable('x-powered-by')
+
+const swaggerOptions = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'API Documentation for Project-G',
+			version: '1.0',
+			description: 'Documentaion for all endpoints to be used on this API',
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000/projectg', //cuando el server este en la nube cambiar esto por la url del servicio
+			},
+		],
+		components: {
+			securitySchemes: {
+				Bearer: {
+					type: 'http',
+					scheme: 'bearer',
+					bearerFormat: 'JWT',
+					description: 'Ingresa el token generado para poder realizar solicitudes',
+				},
+			},
+		},
+		security: [
+			{
+				Bearer: [],
+			},
+		],
+	},
+	apis: ['./routes/*.ts'], // Indica la ubicación de tus rutas para generar la documentación
+}
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use('/projectg', baseRouter)
 
 app.use((req, res) => {
 	res.status(404).send({ error: 'invalid route' })
